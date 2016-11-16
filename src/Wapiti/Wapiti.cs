4379b758a421e2d25025f2c0fdf06e7e14e74bec
Christopher Boumenot <boumenot@gmail.com>
2016-11-15 23:21:48 -0800

    Set PATH to influence DLL load path

--- src/Wapiti/Wapiti.cs
+++ src/Wapiti/Wapiti.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -6,6 +8,53 @@ namespace Wapiti
 {
     public class Wapiti
     {
+        private const string DllName = "libwapiti.dll";
+
+        static Wapiti()
+        {
+            Wapiti.SetPathForNativeDllResolution();
+        }
+
+        private static string GetExecutingAssemblyPath()
+        {
+            var assemblyFullName = new Uri(Assembly.GetExecutingAssembly().EscapedCodeBase).LocalPath;
+            var assemblyPath = Path.GetDirectoryName(assemblyFullName);
+
+            return assemblyPath;
+        }
+
+        private static void SetPathForNativeDllResolution()
+        {
+            if (IntPtr.Size != 8)
+            {
+                const string message = "Wapiti.NET only support 64-bit processes!";
+                throw new ArgumentOutOfRangeException(message);
+            }
+
+            var assemblyPath = Wapiti.GetExecutingAssemblyPath();
+            Wapiti.SetPathForNativeDllResolution(assemblyPath, "native", "x64");
+            // Support for LINQPad
+            Wapiti.SetPathForNativeDllResolution(assemblyPath, "..", "..", "native", "x64");
+        }
+
+        private static void SetPathForNativeDllResolution(params string[] path)
+        {
+            const string PATH = "PATH";
+            var nativeDllPath = Path.Combine(path);
+
+            if (!File.Exists(Path.Combine(nativeDllPath, Wapiti.DllName)))
+            {
+                return;
+            }
+
+            var envPath = Environment.GetEnvironmentVariable(PATH);
+            if (envPath.IndexOf(nativeDllPath, StringComparison.OrdinalIgnoreCase) < 0)
+            {
+                var newPath = $"{nativeDllPath};{envPath}";
+                Environment.SetEnvironmentVariable(PATH, newPath);
+            }
+        }
+
         private Wapiti() {}
 
         public string Label(WapitiModel model, string path)
