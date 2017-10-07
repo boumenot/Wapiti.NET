3886783c3f4bc936a178a066ac8a26408ab52672
Christopher Boumenot <boumenot@gmail.com>
2017-10-06 23:22:44 -0700

    Common overrides for training

--- src/Wapiti/TrainOpt.cs
+++ src/Wapiti/TrainOpt.cs
@@ -11,6 +11,48 @@
             this.opt.Algo = trainAlgo.ToString();
         }
 
+        public TrainOpt SetL1Penalty(double rho1)
+        {
+            this.opt.Rho1 = rho1;
+            return this;
+        }
+
+        public TrainOpt SetL2Penalty(double rho2)
+        {
+            this.opt.Rho2 = rho2;
+            return this;
+        }
+
+        public TrainOpt SetCompact()
+        {
+            this.opt.Compact = true;
+            return this;
+        }
+
+        public TrainOpt SetSparse()
+        {
+            this.opt.Sparse = true;
+            return this;
+        }
+
+        public TrainOpt SetConvergenceWindowSize(uint objwin)
+        {
+            this.opt.ObjWin = objwin;
+            return this;
+        }
+
+        public TrainOpt SetStopWindowSize(uint stopwin)
+        {
+            this.opt.StopWin = stopwin;
+            return this;
+        }
+
+        public TrainOpt SetStopEpsilon(double stopeps)
+        {
+            this.opt.StopEps = stopeps;
+            return this;
+        }
+
         public Opt Get()
         {
             return this.opt;
---
+++ test/Wapiti.Test/TrainOptTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Wapiti.Test
+{
+    public partial class TrainOptTest
+    {
+        [Fact]
+        public void CommonOverrides()
+        {
+            var testSubject = TrainLimitedMemoryBfgsOpt.Train()
+                .SetL1Penalty(1.01)
+                .SetL2Penalty(2.02)
+                .SetCompact()
+                .SetSparse()
+                .SetConvergenceWindowSize(303)
+                .SetStopWindowSize(404)
+                .SetStopEpsilon(5.05)
+                .Get();
+
+            testSubject.Rho1.Should().Be(1.01);
+            testSubject.Rho2.Should().Be(2.02);
+            testSubject.Compact.Should().BeTrue();
+            testSubject.Sparse.Should().BeTrue();
+            testSubject.ObjWin.Should().Be(303);
+            testSubject.StopWin.Should().Be(404);
+            testSubject.StopEps.Should().Be(5.05);
+        }
+    }
+}
