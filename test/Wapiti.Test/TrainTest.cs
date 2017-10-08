5fe78e00f091d7f02f756029954bd1677aa6bfcc
Christopher Boumenot <boumenot@gmail.com>
2017-10-08 14:30:49 -0700

    Support for training

---
+++ test/Wapiti.Test/TrainTest.cs
@@ -0,0 +1,28 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace Wapiti.Test
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class TrainTest
+    {
+        [Fact]
+        public void GrobidDateModelTest()
+        {
+            var opt = TrainLimitedMemoryBfgsOpt.Train()
+                .SetStopEpsilon(1e-5)
+                .SetStopWindowSize(20)
+                .SetMaxIterations(2000)
+                .Get();
+
+            var trainer = new WapitiTrainer();
+            var result = trainer.Train(
+                opt,
+                "date.training.txt".GetResourceStream(),
+                "date.template.txt".GetResourceStream());
+
+            Approvals.Verify(result);
+        }
+    }
+}
